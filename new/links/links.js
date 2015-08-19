1beee0a3a7c777c673a57dbf700387edb87b1fdc
Georgios Tsakalos <ozmax.planet@gmail.com>
2015-08-19 18:25:49 +0300

    bad initial implementation of link addition

--- new/links/links.js
+++ new/links/links.js
@@ -3,17 +3,39 @@ function(authService, $http){
     this_ = this;
     url = "http://ozmaxplanet.com:8000/links/"
     headers = {'Authorization': "Token " + authService.auth_token};
+    this.showForm = true;
+    this_ = this;
+
     this.getLinks = function(){
         $http.get(url, {'headers': headers}).
             then(function(response){
                 this_.links = response.data;
-                console.log(response.data);
             },
             function(response){
                 console.log(response);
             });
     };
+    
+    cat_url = "http://ozmaxplanet.com:8000/categories/";
+    this.getCategories = function(){
+        $http.get(cat_url, {'headers': headers}).
+            then(function(response){
+                this_.categories = response.data;
+            },
+            function(response){
+            
+            });
+    }
+
+    this.submitLink = function(){
+        data = {
+            'url': this.reg_url,
+            'categories': this.reg_categories
+        };    
+        console.log(data);
+    };
+
     this.getLinks();
-     
+    this.getCategories(); 
 }]);
 
