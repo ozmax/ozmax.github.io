d6264edad7dbdf8eafea3442b1e6b43ed8627bfb
Georgios Tsakalos <ozmax.planet@gmail.com>
2015-08-19 13:36:25 +0300

    clean and refine login controller and html

--- new/login/login.js
+++ new/login/login.js
@@ -1,14 +1,55 @@
-angular.module('link-app').controller('LoginController',['authService', '$location', '$cookies', '$http', function(authService, $location, $cookies, $http){
-    base_url = "http://ozmaxplanet.com:8000"
-    register_url = base_url + '/auth/register/'
+angular.module('link-app').controller('LoginController',['authService', '$location', '$cookies', '$http',
+function(authService, $location, $cookies, $http){
+    base_url = "http://ozmaxplanet.com:8000";
+    register_url = base_url + '/auth/register/';
     login_url = base_url + "/auth/login/";
     this.notReg = true;
     this.loginUsername = '';
     this.loginPassword = '';
+    this.currentForm = 'loginform';
+    var loginFields = ['username', 'password', 'non_field_errors'];
+    var registerFields = ['username', 'password', 'email'];
     this_ = this;
 
-    loginFields = ['username', 'password', 'non_field_errors'];
+    this.cleanErrors = function(){
+        for (var i=0; i<loginFields.length; i++){
+            this['er_'+loginFields[i]] = '';
+            this['er_reg_'+registerFields[i]] = '';
+        }
+    };
+
+    this.cleanPassword = function(){
+        this.loginPassword = '';
+        this.reg_password = '';
+    };
+
+    this.swapForm = function(){
+        this.cleanErrors();
+        this.cleanPassword();
+        if (this.currentForm == 'loginform'){
+            this.currentForm = 'registerform';
+        }
+        else{
+            this.currentForm = 'loginform';
+        }
+    };
+
+    this.swapFormAndNoMessage = function(){
+        this.swapForm();
+        if (!this.notReg){
+            this.notReg = true;
+            this.loginUsername = this.reg_username;
+            for (var i=0; i<registerFields.length; i++){
+                this['reg_'+registerFields[i]] = '';
+            }
+        }
+    };
+
     this.login = function(credentials){
+        credentials = {
+            'username': this.loginUsername,
+            'password': this.loginPassword
+        };
         $http.post(login_url, credentials).
             then(function(response){
                 authService.auth_token = response.data.auth_token;
@@ -24,60 +65,10 @@ angular.module('link-app').controller('LoginController',['authService', '$locati
                         this_['er_'+loginFields[i]] = data[loginFields[i]][0];
                     }
                 }
-                this_.form_error = true;
             });
     };
 
-    this.currentForm = 'loginform';
-    this.swapForm = function(){
-        if (this.currentForm == 'loginform'){
-            this.currentForm = 'registerform';
-        }
-        else{
-            this.currentForm = 'loginform';
-        }
-    };
-
-    var fieldNames = ['username', 'password', 'email'];
-    
-    this.swapFormAndNoMessage = function(){
-        this.swapForm();
-        if (!this.notReg){
-            this.notReg = true;
-            this.loginUsername = this.reg_username;
-            for (var i=0; i<fieldNames.length; i++){
-                this['reg_'+fieldNames[i]] = '';
-            }
-        }
-    };
-    
-    this.submitLogin = function(){
-        credentials = {
-            'username': this.loginUsername,
-            'password': this.loginPassword
-        };
-        this.login(credentials);
-    };
-    
-    this.register = function(regData){
-        $http.post(register_url, regData).
-            then(function(response){
-                this_.notReg = false;
-            },
-            function(response){
-            for (var i=0; i<fieldNames.length; i++){
-                this_['er_reg_'+fieldNames[i]] = '';
-            }
-                data = response.data;
-                for (var i=0; i<fieldNames.length; i++){
-                    if (data[fieldNames[i]]){
-                        this_['er_reg_'+fieldNames[i]]=data[fieldNames[i]][0];
-                    }
-                }
-            });
-    };
-    
-    this.submitRegister = function(){
+    this.register = function(){
         regData = {
             'username': this.reg_username,
             'first_name': this.reg_fname,
@@ -85,6 +76,20 @@ angular.module('link-app').controller('LoginController',['authService', '$locati
             'email': this.reg_email,
             'password': this.reg_password
         };
-        this.register(regData);
+        $http.post(register_url, regData).
+            then(function(response){
+                this_.notReg = false;
+            },
+            function(response){
+            for (var i=0; i<registerFields.length; i++){
+                this_['er_reg_'+registerFields[i]] = '';
+            }
+                data = response.data;
+                for (var i=0; i<registerFields.length; i++){
+                    if (data[registerFields[i]]){
+                        this_['er_reg_'+registerFields[i]]=data[registerFields[i]][0];
+                    }
+                }
+            });
     };
 }]);
