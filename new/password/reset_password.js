569e2ce851ed93a4f47161290854b14793336fd6
Georgios Tsakalos <ozmax.planet@gmail.com>
2015-09-02 17:26:39 +0300

    add error visuals for password reset

--- new/password/reset_password.js
+++ new/password/reset_password.js
@@ -4,20 +4,42 @@ $location, $routeParams, $route){
 
     uid = $routeParams.uid;
     token = $routeParams.token;
+    var this_ = this;
+    this.preReset = true;
+    passFields = ['new_password', 're_new_password', 'non_field_errors'];
+
     this.resetPassword = function(){
+        this.loading = true;
         data = {
             'uid': uid,
             'token': token,
             'new_password': this.new_password,
             're_new_password': this.re_new_password
         };
-        console.log(data);
         url = "http://ozmaxplanet.com:8000/auth/password/reset/confirm/"
-        $http.post(url,data).then(function(response){
-            console.log(response);
-        },function(response){
-            console.log(response);
+        $http.post(url,data).then(
+            function(response){
+                this_.preReset = false;
+                this_.loading = false;
+                this_.message = "Password succesfully changed!"
+            }
+            ,function(response){
+                data = response.data
+                console.log(data)
+                for (var i=0; i<passFields.length; i++){
+                    this_['er_'+passFields[i]] = '';
+                    if (data[passFields[i]]){
+                        this_['er_'+passFields[i]] = data[passFields[i]][0];
+                    }
+                }
+                console.log(this_)
+                this_.preReset = true;
+                this_.loading = false;
         });
+    };
+
+    this.returnToLogin = function(){
+        $location.path('/login')
     };
 }]);
 
