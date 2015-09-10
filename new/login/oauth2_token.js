2aba34585ad45ab42de1969d350cf72c3435d1ce
Georgios Tsakalos <ozmax.planet@gmail.com>
2015-09-10 14:44:35 +0300

    save token in cookies after oauth2 login

--- new/login/oauth2_token.js
+++ new/login/oauth2_token.js
@@ -1,10 +1,11 @@
 
 angular.module('link-app').controller('getTokenController', ['$scope', '$http', '$location', 
-'$routeParams', '$route', 'authService', function($scope, $http, $location,
-$routeParams, $route, authService){
+'$routeParams', '$route', '$cookies', 'authService', function($scope, $http, $location,
+$routeParams, $route, $cookies, authService){
     token = $routeParams.token;
     authService.isAuthenticated = true;
     authService.auth_token = token;
+    $cookies.put('token', token);
     $location.path('links');
 
 }]);
